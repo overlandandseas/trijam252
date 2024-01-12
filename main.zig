fa8376d7d36783f26e89232934d04b152bdfa41b
byoung <byoung@fastly.com>
2024-01-11 19:43:46 -0600

    try with assets

--- main.zig
+++ main.zig
@@ -10,7 +10,7 @@ pub fn main() void {
     ray.InitWindow(SCREEN_W, SCREEN_H, "zrayjam");
     ray.SetTargetFPS(60);
 
-    var target = ray.LoadRenderTexture(SCREEN_W / 2, SCREEN_H / 2);
+    var target = ray.LoadRenderTexture(SCREEN_W / 4, SCREEN_H / 4);
 
     const txt = "Zello, Rayworld!";
     const fsz = 30;
@@ -22,9 +22,11 @@ pub fn main() void {
         .fovy = 45,
         .projection = ray.CAMERA_PERSPECTIVE,
     };
-
     var cube_msh = ray.GenMeshCube(1, 1, 1);
     var mod = ray.LoadModelFromMesh(cube_msh);
+    _ = mod;
+
+    var phone = ray.LoadModel("res/phone.glb");
 
     while (!ray.WindowShouldClose()) {
         if (ray.IsKeyDown(ray.KEY_W)) {
@@ -52,15 +54,18 @@ pub fn main() void {
             {
                 var time: f32 = @floatCast(ray.GetTime());
                 var mtx = ray.MatrixIdentity();
-                var rot = ray.MatrixRotate(.{ .x = 0.97, .y = 0.53, .z = 0.23 }, time);
+                // var rot = ray.MatrixRotate(.{ .x = 0.97, .y = 0.53, .z = 0.23 }, time);
                 var trn = ray.MatrixTranslate(5 + @sin(time / 2), 5 + @sin(time / 3), 5 + @sin(time / 4));
-                mtx = ray.MatrixMultiply(mtx, rot);
+                // mtx = ray.MatrixMultiply(mtx, rot);
                 mtx = ray.MatrixMultiply(mtx, trn);
 
+                phone.transform = mtx;
+                ray.DrawModel(phone, .{}, 1, ray.RED);
+
                 ray.DrawGrid(10, 10);
-                mod.transform = mtx;
-                ray.DrawModel(mod, .{}, 1, ray.GREEN);
-                ray.DrawModelWires(mod, .{}, 1, ray.DARKGREEN);
+                // mod.transform = mtx;
+                // ray.DrawModel(mod, .{}, 1, ray.GREEN);
+                // ray.DrawModelWires(mod, .{}, 1, ray.DARKGREEN);
             }
             ray.EndMode3D();
         }
@@ -72,7 +77,7 @@ pub fn main() void {
             ray.ClearBackground(ray.RAYWHITE);
 
             // blit 3D to screen
-            ray.DrawTexturePro(target.texture, .{ .x = 0, .y = 0, .width = SCREEN_W / 2, .height = SCREEN_H / 2 }, .{ .x = 0, .y = 0, .width = SCREEN_W, .height = SCREEN_H }, .{ .x = 0, .y = 0 }, 0.0, ray.WHITE);
+            ray.DrawTexturePro(target.texture, .{ .x = 0, .y = 0, .width = SCREEN_W / 4, .height = SCREEN_H / 4 }, .{ .x = 0, .y = 0, .width = SCREEN_W, .height = SCREEN_H }, .{ .x = 0, .y = 0 }, 0.0, ray.WHITE);
 
             var w = ray.MeasureText(txt, fsz);
             ray.DrawText(txt, SCREEN_W / 2 - @divTrunc(w, 2), 100, fsz, ray.GRAY);
